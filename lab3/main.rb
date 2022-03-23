ff1e2075d3a7edc81ffda8e79e5704f0f4e10c2b
valerchik <kovalskiy0303@mail.ru>
2022-03-23 18:18:03 +0300

    do third lab

---
+++ lab3/main.rb
@@ -0,0 +1,31 @@
+def replacing (str)
+    if str[str.length-2, str.length-1] == "CS"
+      puts 2 ** str.length
+    else puts str.reverse
+    end
+  end
+  
+  def pokemonsList ()
+    arr = []
+    puts "How many pokemons do you want to add?"
+    quantity = gets.to_i
+    if quantity > 0
+      until quantity == 0
+        puts "Input name:"
+        name = gets.chomp
+        puts "Input color:"
+        color = gets.chomp
+        arr.push({name: name, color: color})
+        quantity -= 1
+      end
+    else puts "No pokemons to add"
+    end
+    arr.each {|hash| puts hash}
+  end
+  
+  
+  str = gets.chomp
+  replacing(str)
+  pokemonsList()
+        
+    
